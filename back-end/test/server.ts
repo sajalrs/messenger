62e4b647c1771d1de2ece425df972dfc76e4225d
sajalrs <sajalsatyal@gmail.com>
2021-04-30 08:40:33 -0500

    test endpoint runs monolithic graphql server

--- back-end/test/server.ts
+++ back-end/test/server.ts
@@ -1,27 +1,75 @@
-import express from "express";
+import { ApolloServer, gql } from "apollo-server";
 import ConnectionPool from "./db";
-// Constants
+
 const PORT = 8080;
-const HOST = "0.0.0.0";
 
-// App
-const app = express();
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-app.post("/", async (req, res) => {
-  try {
-    const { firstName, lastName } = req.body;
-    const newUser = await ConnectionPool.query(
-      "INSERT INTO users (first_name, last_name) VALUES ($1, $2) RETURNING *",
-      [firstName, lastName]
-    );
-
-    res.json(newUser.rows[0]);
-  } catch (err) {
-    console.log(err.message);
+const typeDefs = gql`
+  type User {
+    user_id: ID!
+    first_name: String
+    last_name: String
   }
+
+  type Query {
+    user(user_id: ID!): User
+    users: [User]
+  }
+
+  type Mutation {
+    register(first_name: String!, last_name: String!): User!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    user: async (_: any, args: { user_id: String }, __: any) => {
+      try {
+        const { user_id } = args;
+        const user = await ConnectionPool.query(
+          "SELECT * FROM users WHERE user_id = $1",
+          [user_id]
+        );
+        return user.rows[0];
+      } catch (err) {
+        console.log(err);
+      }
+    },
+
+    users: async () => {
+      try {
+        const allUsers = await ConnectionPool.query("SELECT * FROM users");
+        return allUsers.rows;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+
+  Mutation: {
+    register: async (
+      _: any,
+      args: { first_name: String; last_name: String },
+      __: any
+    ) => {
+      try {
+        const { first_name, last_name } = args;
+        const newUser = await ConnectionPool.query(
+          "INSERT INTO users (first_name, last_name) VALUES ($1, $2) RETURNING *",
+          [first_name, last_name]
+        );
+        return newUser.rows[0];
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+server.listen(PORT).then(() => {
+  console.log(`GraphQL Server Running`);
+});
